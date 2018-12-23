ebb13def28285093ef699f3a6608d08e8d846f32
mohit kumar (KUMARMOH) <mohit.kumar3@amdocs.com>
2018-12-24 01:13:42 +0530

    Modified homepage to show basic questions layout

--- ques_app/views.py
+++ ques_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .models import Question, Answer
+
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'ques_app/home.html'
+    
+    def get_context_data(self, **kwargs):
+        '''
+        Overiding the context date sent to template
+        '''
+        context = super().get_context_data(**kwargs)
+        context['questions'] = Question.objects.all()
+        return context
