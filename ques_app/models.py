dd3f5d734e06400cd4bfa079b4ac567d7775817d
mohit kumar (KUMARMOH) <mohit.kumar3@amdocs.com>
2018-12-24 00:35:33 +0530

    Added Question Model

--- ques_app/models.py
+++ ques_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    '''
+    Structure of a Question Model
+    '''
+    text = models.TextField(max_length=1000)
+    author = models.CharField(max_length=50)
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        '''String representation of a model object'''
+        return self.text[:20]+'...'
